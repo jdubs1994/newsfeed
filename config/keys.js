90b5fdf66558ae5105f2e3880b936dd4a245f695
JamesDuBois1994 <duboisprogramming@gmail.com>
2018-12-17 16:55:32 -0500

    Pulled in boilerplate Google OAuth passport code that I begin most of my projects with. I configured the boiler plate to work with my specific google clientID/Secret for this application.

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://james:password1@ds237574.mlab.com:37574/newsfeed',
+  googleClientID:'109369990946-vkfg5hm9gm4f117d751hiprkkundlq09.apps.googleusercontent.com',
+  googleClientSecret:'b5x7nqdnbtiJMH5ZNk6lztya'
+}
