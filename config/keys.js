bfb935e75d3a54abc44ddde3fb33b0e40aa0e886
James DuBois <duboisprogramming@gmail.com>
2018-12-19 12:29:46 -0500

    Getting ready to submit, updated db username and password to more secure ones.

--- config/keys.js
+++ config/keys.js
@@ -1,16 +1,16 @@
 module.exports = {
   development: {
-    mongoURI:'mongodb://dev:password1@ds263837.mlab.com:63837/newsfeed-dev',
+    mongoURI:'mongodb://secure2131213123123131767:Xf0nOP49Yvr1ALxv8it5@ds263837.mlab.com:63837/newsfeed-dev',
     googleClientID:'109369990946-vkfg5hm9gm4f117d751hiprkkundlq09.apps.googleusercontent.com',
     googleClientSecret:'b5x7nqdnbtiJMH5ZNk6lztya'
   },
   test: {
-    mongoURI:'mongodb://james:password1@ds237574.mlab.com:37574/newsfeed',
+    mongoURI:'mongodb://secure4241290840812340982130:m0h8rC3Kxszm84LZgaka@ds237574.mlab.com:37574/newsfeed',
     googleClientID:'109369990946-vkfg5hm9gm4f117d751hiprkkundlq09.apps.googleusercontent.com',
     googleClientSecret:'b5x7nqdnbtiJMH5ZNk6lztya'
   },
   production: {
-    mongoURI:'mongodb://james:password1@ds237574.mlab.com:37574/newsfeed',
+    mongoURI:'mongodb://secure2341241234124213940481:N7k1p3Wapza22GfzMLDz@ds237574.mlab.com:37574/newsfeed',
     googleClientID:'109369990946-vkfg5hm9gm4f117d751hiprkkundlq09.apps.googleusercontent.com',
     googleClientSecret:'b5x7nqdnbtiJMH5ZNk6lztya'
   }
